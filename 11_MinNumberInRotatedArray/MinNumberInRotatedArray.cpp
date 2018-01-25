4149bd63f22afedaf6fd87e455e6868c875af53e
lwj <lwj102153@gmail.com>
2018-01-25 14:32:49 +0800

    add a new solution for MinNumberInRotateArray

--- 11_MinNumberInRotatedArray/MinNumberInRotatedArray.cpp
+++ 11_MinNumberInRotatedArray/MinNumberInRotatedArray.cpp
@@ -57,6 +57,52 @@ int Min(int* numbers, int length)
     return numbers[indexMid];
 }
 
+
+/* 辅助函数,我们也可以用标准库中的min函数 */
+int min(int a, int b)
+{
+    return a<b?a:b;
+}
+
+/* 一种新的解法,使用递归,这样我们仍然可以得到log(n)的时间复杂度 */
+int Min2(int *numbers, int length)
+{
+    if(numbers == nullptr || length <= 0)
+        throw new std::exception();
+
+    /* 这是防止类似于{1,1,1}的情况,原解法不用考虑这种情况 */
+    if(length == 1)
+        return numbers[0];
+ 
+    int index1 = 0;
+    int index2 = length - 1;
+    int indexMid = index1;
+    while(numbers[index1] >= numbers[index2])
+    {
+        // 如果index1和index2指向相邻的两个数，
+        // 则index1指向第一个递增子数组的最后一个数字，
+        // index2指向第二个子数组的第一个数字，也就是数组中的最小数字
+        if(index2 - index1 == 1)
+        {
+            indexMid = index2;
+            break;
+        }
+ 
+        // 如果下标为index1、index2和indexMid指向的三个数字相等，
+        // 我们递归调用自身
+        indexMid = (index1 + index2) / 2;
+        if(numbers[index1] == numbers[index2] && numbers[indexMid] == numbers[index1])
+            return min(Min2(numbers, length/2), Min2(numbers + length/2,length - static_cast<int>(length/2)));
+
+        // 缩小查找范围
+        if(numbers[indexMid] >= numbers[index1])
+            index1 = indexMid;
+        else if(numbers[indexMid] <= numbers[index2])
+            index2 = indexMid;
+    }
+    return numbers[indexMid];
+}
+
 int MinInOrder(int* numbers, int index1, int index2)
 {
     int result = numbers[index1];
